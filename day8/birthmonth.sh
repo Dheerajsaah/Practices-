75d209f35a4f317f7900260302aa56c5063e5dad
Dheerajsaah <109459249+Dheerajsaah@users.noreply.github.com>
2022-08-02 12:53:43 +0530

    Add files via upload

---
+++ day8/birthmonth.sh
@@ -0,0 +1,35 @@
+#! /bin/bash/ -x
+
+ MAXCOUNT=25
+ count=1
+ 
+ while [ "$count" -le $MAXCOUNT ]; do
+   yr92[$count]=$(((RANDOM%12+1)))
+   yr93[$count]=$(((RANDOM%12+1)))
+ let "count += 1"
+ done
+ 
+ echo "Birth month of individuals born in yr 92 are" $(yr92[@)
+ echo""
+ echo "And individuals are "${!yr92[@]}
+ echo""
+
+ echo "Birth month of individuals born in yr 93 are " $(yr93[@)
+ echo""
+ echo "And individuals are "${!yr93[@]}
+
+for((i=0; i < ${#yr92[@]}; i++))
+ do
+if [[ "${yr92[i]}" -eq 8 ]]
+  then
+       echo""
+       echo "August is birth month of"  ${!yr92[i]}
+ elif [[ "${yr93[i]}" -eq 8 ]]
+   then
+       echo""
+       echo "August is birth month of" ${!yr93[i]}
+ fi
+  done
+
+
+
